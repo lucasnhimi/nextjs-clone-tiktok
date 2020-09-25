a3cfc0f9fedd00deb50e9dc01111e4ba700cbaa4
lucaslonguinhos <lucaslonguinhos@gmail.com>
2020-09-25 11:12:48 -0300

    Fix suggestions users

--- src/components/Suggestions/index.js
+++ src/components/Suggestions/index.js
@@ -17,97 +17,97 @@ import RecommendCard from '../RecommendCard';
 const people = [
   {
     name: 'Neymar Jr',
-    userName: '@neymarjr',
+    username: '@neymarjr',
     avatar:
       'https://p77-sg.tiktokcdn.com/img/musically-maliva-obj/8a7686c7081061b6771ce7f3e6dee9cc~c5_100x100.jpeg',
   },
   {
     name: 'Felipe Neto',
-    userName: '@felipeneto',
+    username: '@felipeneto',
     avatar:
       'https://p77-sg.tiktokcdn.com/img/musically-maliva-obj/1647961242038278~c5_100x100.jpeg',
   },
   {
     name: 'Anitta',
-    userName: '@anitta',
+    username: '@anitta',
     avatar:
       'https://p77-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/cbf8d6144770083807ece585afc668ce.jpeg?x-expires=1601125200&x-signature=eEiuYnl%2Fl3JG0Glicyoe9lX8M0A%3D',
   },
   {
     name: 'Isis Valverde',
-    userName: '@isisvalverde',
+    username: '@isisvalverde',
     avatar:
       'https://p77-sg.tiktokcdn.com/img/musically-maliva-obj/5425b455c73f7d399d7538e8b059d9ef~c5_100x100.jpeg',
   },
   {
     name: 'Neymar Jr',
-    userName: '@neymarjr',
+    username: '@neymarjr',
     avatar:
       'https://p77-sg.tiktokcdn.com/img/musically-maliva-obj/8a7686c7081061b6771ce7f3e6dee9cc~c5_100x100.jpeg',
   },
   {
     name: 'Felipe Neto',
-    userName: '@felipeneto',
+    username: '@felipeneto',
     avatar:
       'https://p77-sg.tiktokcdn.com/img/musically-maliva-obj/1647961242038278~c5_100x100.jpeg',
   },
   {
     name: 'Anitta',
-    userName: '@anitta',
+    username: '@anitta',
     avatar:
       'https://p77-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/cbf8d6144770083807ece585afc668ce.jpeg?x-expires=1600527600&x-signature=7xKDPGFet6e0H%2FUZkqEovBS%2FDNw%3D',
   },
   {
     name: 'Isis Valverde',
-    userName: '@isisvalverde',
+    username: '@isisvalverde',
     avatar:
       'https://p77-sg.tiktokcdn.com/img/musically-maliva-obj/5425b455c73f7d399d7538e8b059d9ef~c5_100x100.jpeg',
   },
   {
     name: 'Neymar Jr',
-    userName: '@neymarjr',
+    username: '@neymarjr',
     avatar:
       'https://p77-sg.tiktokcdn.com/img/musically-maliva-obj/8a7686c7081061b6771ce7f3e6dee9cc~c5_100x100.jpeg',
   },
   {
     name: 'Felipe Neto',
-    userName: '@felipeneto',
+    username: '@felipeneto',
     avatar:
       'https://p77-sg.tiktokcdn.com/img/musically-maliva-obj/1647961242038278~c5_100x100.jpeg',
   },
   {
     name: 'Anitta',
-    userName: '@anitta',
+    username: '@anitta',
     avatar:
       'https://p77-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/cbf8d6144770083807ece585afc668ce.jpeg?x-expires=1600527600&x-signature=7xKDPGFet6e0H%2FUZkqEovBS%2FDNw%3D',
   },
   {
     name: 'Isis Valverde',
-    userName: '@isisvalverde',
+    username: '@isisvalverde',
     avatar:
       'https://p77-sg.tiktokcdn.com/img/musically-maliva-obj/5425b455c73f7d399d7538e8b059d9ef~c5_100x100.jpeg',
   },
   {
     name: 'Neymar Jr',
-    userName: '@neymarjr',
+    username: '@neymarjr',
     avatar:
       'https://p77-sg.tiktokcdn.com/img/musically-maliva-obj/8a7686c7081061b6771ce7f3e6dee9cc~c5_100x100.jpeg',
   },
   {
     name: 'Felipe Neto',
-    userName: '@felipeneto',
+    username: '@felipeneto',
     avatar:
       'https://p77-sg.tiktokcdn.com/img/musically-maliva-obj/1647961242038278~c5_100x100.jpeg',
   },
   {
     name: 'Anitta',
-    userName: '@anitta',
+    username: '@anitta',
     avatar:
       'https://p77-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/cbf8d6144770083807ece585afc668ce.jpeg?x-expires=1600527600&x-signature=7xKDPGFet6e0H%2FUZkqEovBS%2FDNw%3D',
   },
   {
     name: 'Isis Valverde',
-    userName: '@isisvalverde',
+    username: '@isisvalverde',
     avatar:
       'https://p77-sg.tiktokcdn.com/img/musically-maliva-obj/5425b455c73f7d399d7538e8b059d9ef~c5_100x100.jpeg',
   },
